e091f61bde928a47913bcb0be3595fa0c837ad71
vishal <vishualhat@gmail.com>
2015-10-20 18:27:48 +0530

    20.10

--- src/org/JavaCollection/Map/HashMap1.java
+++ src/org/JavaCollection/Map/HashMap1.java
@@ -12,8 +12,18 @@ public class HashMap1 {
 		employee.put(2, "emp2");
 		employee.put(3, "Employee 3");
 
+		System.out.println("Display entire keys and Values of map:");
 		System.out.println(employee);
-
+		System.out.println();
+		System.out.println("Check for presence of given value using containsValue() ");
+		System.out.println(employee.containsValue("emp2"));
+		System.out.println();
+		System.out.println("Get value mapped to certain Key :");
+		System.out.println(employee.get(3));
+		System.out.println("Get Value at specified Key and if absent Print Default Value or message");
+		System.out.println(employee.getOrDefault(2, "No Value at given key"));
+		System.out.println(employee.getOrDefault(5, "No Value at given key"));
+		
 	}
 
 	public static void main(String[] args) {
